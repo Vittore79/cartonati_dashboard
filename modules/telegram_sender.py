e0ba5d59383aa304f1ec7446194c2f9cf01f851e
Vittore Maltese <rastotv2000@gmail.com>
2026-05-28 15:02:35 +0200

    fixed telegram sender

--- modules/telegram_sender.py
+++ modules/telegram_sender.py
@@ -4,15 +4,12 @@ BOT_TOKEN = "8312714597:AAGXOyaW8b1k_PBS0OYf92MdgoDP2fImJXs"
 CHAT_ID = "7494998558"
 
 def send_message(text):
- 
-url = (
-    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-)
+
+```
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 payload = {
-
     "chat_id": CHAT_ID,
-
     "text": text
 }
 
@@ -24,13 +21,10 @@ try:
         timeout=10
     )
 
-    print(
-        "Messaggio Telegram inviato"
-    )
+    print("Messaggio Telegram inviato")
 
 except Exception as error:
 
-    print(
-        "Errore Telegram:",
-        error
-    )
+    print("Errore Telegram:", error)
+```
+
