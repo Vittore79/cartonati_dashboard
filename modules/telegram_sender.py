f3c062bbd0fd8f456867786aafb36a96022937b6
Vittore79 <vittore_malt@libero.it>
2026-05-29 10:11:07 +0200

    Update telegram_sender.py

--- modules/telegram_sender.py
+++ modules/telegram_sender.py
@@ -3,26 +3,26 @@ import requests
 BOT_TOKEN = "8312714597:AAGXOyaW8b1k_PBS0OYf92MdgoDP2fImJXs"
 CHAT_ID = "7494998558"
 
+
 def send_message(text):
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-payload = {
-    "chat_id": CHAT_ID,
-    "text": text
-}
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
 
-try:
+    try:
 
-    requests.post(
-        url,
-        json=payload,
-        timeout=10
-    )
+        requests.post(
+            url,
+            json=payload,
+            timeout=10
+        )
 
-    print("Messaggio Telegram inviato")
+        print("Messaggio Telegram inviato")
 
-except Exception as error:
+    except Exception as error:
 
-    print("Errore Telegram:", error)
-
+        print("Errore Telegram:", error)
